0aa49c88f4bd90f77c609d9ce9af6bf2ba903659
jojae38 <58541374+jojae38@users.noreply.github.com>
2022-01-20 08:35:54 +0900

    Update function.cpp

--- lab01/lab01/function.cpp
+++ lab01/lab01/function.cpp
@@ -2,11 +2,11 @@
 
 int Ver1::div_fun(int a,int b)
 {
-	std::cout << "¸òÀº : " << a / b << std::endl;
+	std::cout << "Val : " << a / b << std::endl;
 	return a / b;
 }
 int Ver2::div_fun(int a, int b)
 {
-	std::cout << "¸òÀº : " << a / b << "³ª¸ÓÁö´Â : " << a % b << std::endl;
+	std::cout << "Val : " << a / b << "Res : " << a % b << std::endl;
 	return a / b;
 }
