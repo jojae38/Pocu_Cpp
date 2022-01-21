0264748a7a991b9ad26f913244a3830b795454d3
jojae38 <58541374+jojae38@users.noreply.github.com>
2022-01-21 15:15:35 +0900

    Update lab03.cpp

--- lab03/src/lab03.cpp
+++ lab03/src/lab03.cpp
@@ -8,4 +8,10 @@ int main()
 	/*입력을 받는 방식 - 입력 연산자*/
 	cin >> hours;
 	cout << "Today I studied for " << hours <<"hours"<< endl;
+	
+	char firstName[4];
+	/*이것도 C 처럼 4글자가 들어가면 안된다 마지막은 /NULL문자를 생각해야 해서 3글자가 들어가던지 []를 5로 바꿔준다.*/
+	cin >> firstName;
+	cout <<firstName<<endl;
+	
 }
